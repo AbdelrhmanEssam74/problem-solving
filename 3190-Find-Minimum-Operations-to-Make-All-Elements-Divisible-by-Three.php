b1c65b688b1c5574cf1d3bab5837146467eaee20
Abdelrhman Essam <89538266+AbdelrhmanEssam74@users.noreply.github.com>
2024-07-08 13:02:24 +0300

    [3190] [Time Beats: 100.00%] [Memory Beats: 92.59%] - LeetPush

--- 3190-Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three.php
+++ 3190-Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three.php
@@ -5,9 +5,11 @@ class Solution {
      * @return Integer
      */
     function minimumOperations($nums) {
-        $num_operations  = 0;
+        $num_operations  = 0; // number of the operations
         foreach ($nums as $num) :
             if ($num % 3 != 0) :
+            // the number need to be incremented or decremented
+            // so increament the number of operation by 1
                 $num_operations += 1;
             endif;
         endforeach;
